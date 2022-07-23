5b8a9fdf7bb86b810b8d37ba1f0b43d98f80ddb1
dilan.martinez <zzdilan123@gmail.com>
2022-07-23 00:28:03 -0300

    subiendo nosotros

--- site/app.js
+++ site/app.js
@@ -10,5 +10,6 @@ app.get('/detalle',(req,res) => res.sendFile(path.resolve(__dirname,'views','det
 app.get('/carrito',(req,res) => res.sendFile(path.resolve(__dirname,'views','carrito.html')))
 app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
 app.get('/register',(req,res) => res.sendFile(path.resolve(__dirname,'views','register.html')))
+app.get('/nosotros',(req,res) => res.sendFile(path.resolve(__dirname,'views','nosotros.html')))
 
 app.listen(port,() => console.log(`Servidor corriendo en http://localhost:${port}`))
