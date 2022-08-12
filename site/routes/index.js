6eb26a75b6ed6bc366a689dfd5f07ec24f111835
MelanieRosi <MelanieRosi25@gmail.com>
2022-08-12 06:48:54 -0300

    dejo los links del footer funcionando con su ruta respectiva

--- site/routes/index.js
+++ site/routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-let {home,nosotros, error404} = require('../controllers/indexController')
+let {home,nosotros} = require('../controllers/indexController')
 
 router.get('/', home)
 router.get('/nosotros', nosotros)
-/*router.get('*', error404) */
-
 
 module.exports = router
