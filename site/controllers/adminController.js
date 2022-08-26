5ac2b46bd950731b40f44fae41a29f76d844c94e
MelanieRosi <MelanieRosi25@gmail.com>
2022-08-26 16:57:44 -0300

    agrego la dependencia de metodos, y modifico el formulario y rutas/controladores para proximamente dejar la funcionalidad lista

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -10,14 +10,6 @@ module.exports = {
     crear:(req,res) => {
         return res.render('admin/crear')
     },
-    editar:(req,res) => {
-        /* id = +req.params.id
-        let producto = productos.find((elemento) => {
-            return elemento.id == id
-        }) */
-        /* return res.send(producto) Comprobar que esta llegando bien el elemento*/
-        return res.render('admin/editar')
-    },
     nuevo:(req,res) => {
         let {Titulo,Autor,Idioma,Editorial,Tapa,Modelo,Categoria,Precio,Descuento,Stock,Descripcion,Subcategoria} = req.body;
 
@@ -41,5 +33,19 @@ module.exports = {
         guardar(productos)
 
        return res.redirect('/administrador/listar')
+    },
+    editar:(req,res) => {
+        /* id = +req.params.id
+        let producto = productos.find((elemento) => {
+            return elemento.id == id
+        }) */
+        /* return res.send(producto) Comprobar que esta llegando bien el elemento*/
+        return res.render('admin/editar')
+    },
+    actualizar:(req,res) => {
+
+    },
+    destruir:(req,res) => {
+
     }
 }
--- site/routes/administrador.js
+++ site/routes/administrador.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-let {crear,editar,listar,nuevo} = require('../controllers/adminController')
+let {listar,nuevo,crear,editar, actualizar,destruir} = require('../controllers/adminController')
 
 router.get('/listar', listar)
-router.get('/editar', editar)
 
 /*crear producto*/
 router.get('/crear', crear)
 router.post('/crear', nuevo)
+
+/*editar producto*/ 
+router.get('/editar', editar)
+/*router.get('/editar/:id', cambio);*/
+router.put('/editar/:id', actualizar);
+
+/* Eliminar un producto */
+router.delete('/destruir/:id', destruir);
 
 module.exports = router
