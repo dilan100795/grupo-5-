823ea200a213af801bc7b199482ce65a7aa2e654
MelanieRosi <MelanieRosi25@gmail.com>
2022-08-26 03:32:30 -0300

    subo funcionalidad de crear producto nuevo completa redireccionando al listado + dos imagenes de portadas y correcciones en el formulario de crear

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path')
+const productos = require('../data/productos.json');
+const guardar = (dato) => fs.writeFileSync(path.join(__dirname, '../data/productos.json'), JSON.stringify(dato, null, 4), 'utf-8')
+
 module.exports = {
     listar: (req,res) => {
         return res.render('admin/listar')
@@ -14,7 +19,27 @@ module.exports = {
         return res.render('admin/editar')
     },
     nuevo:(req,res) => {
-        let productoCreado = req.body
-        res.send(productoCreado)
+        let {Titulo,Autor,Idioma,Editorial,Tapa,Modelo,Categoria,Precio,Descuento,Stock,Descripcion,Subcategoria} = req.body;
+
+        let productoNuevo = {
+            id : productos[productos.length - 1].id + 1,
+            titulo: Titulo,
+            autor: Autor,
+            idioma: Idioma,
+            editorial: Editorial,
+            tapa: Tapa,
+            modelo: Modelo,
+            categoria: Categoria,
+            precio: Precio,
+            descuento: Descuento,
+            stock: Stock,
+            descripcion: Descripcion,
+            subcategoria: Subcategoria,
+            imagen: ["default-image.jpg"]
+        }
+        productos.push(productoNuevo)
+        guardar(productos)
+
+       return res.redirect('/administrador/listar')
     }
 }
