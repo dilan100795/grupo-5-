914d4c119a7be5e7d93d8150d57e04f2616c6358
dilan.martinez <zzdilan123@gmail.com>
2022-08-27 13:38:16 -0300

    agregoctegorias

--- site/routes/productos.js
+++ site/routes/productos.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-let {carrito,detalle} = require('../controllers/productosController')
+let {carrito,detalle,categorias} = require('../controllers/productosController')
 
 router.get('/carrito', carrito)
 router.get('/detalle/:id',detalle)
-
+router.get('/categorias',categorias)
 module.exports = router
